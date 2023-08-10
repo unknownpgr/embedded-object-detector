d8cf393e1b0c86fc119acda4551e456097724304
UnknownPgr <unknownpgr@gmail.com>
2023-08-10 20:43:23 +0900

    Add center finding logic

--- tester.py
+++ tester.py
@@ -26,6 +26,10 @@ while True:
     # Copy frame
     displayframe = frame.copy()
 
+    box_x_sum = 0
+    box_y_sum = 0
+    box_count = 0
+
     # Slide box
     for y in range(0, HEIGHT - BOX_HEIGHT, STRIDE):
         for x in range(0, WIDTH - BOX_WIDTH, STRIDE):
@@ -38,6 +42,11 @@ while True:
             # Predict
             prediction = model.predict(np.matrix(cropped))
             if prediction[1][0][0] == 1.0:
+                # Calculate center of box
+                box_x_sum += x + BOX_WIDTH / 2
+                box_y_sum += y + BOX_HEIGHT / 2
+                box_count += 1
+
                 # Draw box
                 cv2.rectangle(
                     displayframe,
@@ -46,6 +55,14 @@ while True:
                     (0, 255, 0),
                     2,
                 )
+
+    # Calculate center of mass
+    if box_count > 0:
+        center_x = int(box_x_sum / box_count)
+        center_y = int(box_y_sum / box_count)
+
+        # Draw center of mass
+        cv2.circle(displayframe, (center_x, center_y), 15, (255, 0, 0), -1)
 
     cv2.imshow("frame", displayframe)
     out.write(displayframe)
